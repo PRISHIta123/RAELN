979c62b5e1308caa35281da36fe90568bb749155
Prishita Ray <30746562+PRISHIta123@users.noreply.github.com>
2019-12-17 00:08:42 +0530

    Update feature_extraction.py

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,5 +1,5 @@
 #Recurrent Neural Network and LSTM function for feature extraction from log files- Prishita
 def RNN_LSTM():
 
-#Recurrent Neural Network and LSTM function for feature extraction from log files- Tanmayi and Lahari
+#Principle Component Analysis function for feature extraction from log files- Tanmayi and Lahari
 def PCA():
